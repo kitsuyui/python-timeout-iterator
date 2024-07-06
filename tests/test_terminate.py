496ad451888172f1c4f5b20d0e2345edda2775a5
kitsuyui <kitsuyui@kitsuyui.com>
2024-07-06 13:38:45 +0900

    Enhance test coverage for terminate method
    - Test if the signal sent to the terminate method is appropriate
    - Test that the signal is ignored even if it is sent when the terminate method is not timed out

--- tests/test_terminate.py
+++ tests/test_terminate.py
@@ -1,3 +1,4 @@
+import signal
 import time
 
 import pytest
@@ -22,5 +23,27 @@ def test_terminate_2() -> None:
     for i in terminate(range(10), seconds=3.0):
         results.append(i)
         time.sleep(0.1)
+
+    assert results == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def test_terminate_3() -> None:
+    # Do not timeout if the sent signal is not SIGALRM
+    results = []
+    for i in terminate(range(10), seconds=3.0):
+        results.append(i)
+        time.sleep(0.1)
+        signal.alarm(signal.SIGWINCH)
+
+    assert results == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def test_terminate_4() -> None:
+    # Do not timeout if the signal sent but the time is longer than the timeout
+    results = []
+    for i in terminate(range(10), seconds=3.0):
+        results.append(i)
+        time.sleep(0.1)
+        signal.alarm(signal.SIGALRM)
 
     assert results == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
